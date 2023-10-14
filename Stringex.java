5cbd3c20b9d31aee940ea4f1b6dba258d0f2da48
Arnav Panchal <arnavpanchal27@gmail.com>
2023-10-14 23:43:30 +0530

    Updated-Functions

---
+++ Stringex.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Stringex {
+    public static void main(String[] args) {
+//        String message=greet();
+//        System.out.println(message);
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter your name : ");
+        String name = in.next();
+        System.out.println("Enter your surname : ");
+        String Surname=in.next();
+
+        String personalised = myGreet(name,Surname);
+        System.out.println("Welcome... "+personalised);
+    }
+
+    static String myGreet(String name,String surname){
+        String Fullname = name+" " + surname ;
+        return Fullname;
+    }
+    static String greet(){
+        String greeting="how are you ?";
+        return greeting;
+    }
+}
--- Sum.java
+++ Sum.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        suma();
+//        int anw=suma();
+//        System.out.println("Answer :"+anw);
+        int ans= sum3(12,12);
+        System.out.println("Answer : "+ ans );
     }
 
-    static void suma(){
+    static void suma1(){
         Scanner in=new Scanner(System.in);
         System.out.println("Enter number one :");
         int sum1=in.nextInt();
@@ -13,5 +16,21 @@ public class Sum {
         int sum2=in.nextInt();
         int Sum=sum1+sum2;
         System.out.println("Addition of these 2 numbers is :\n"+Sum);
+    }
+
+    static int suma(){
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter number one :");
+        int sum1=in.nextInt();
+        System.out.println("Enter number two :");
+        int sum2=in.nextInt();
+        int Sum=sum1+sum2;
+        return Sum;
+        // Any statement after return will never execute or read
+    }
+
+    static int sum3(int a,int b){
+        int sum =a + b ;
+        return sum;
     }
 }
